0efe126ca92e00fe584131700e0c723ddf4d5365
jdgarrido <damian.garrido.munoz@gmail.com>
2014-02-20 12:38:44 -0300

    add representer for an organization

---
+++ lib/popit_representers/models/organization.rb
@@ -0,0 +1,7 @@
+require 'popit_representers/representers/organization_representer'
+
+module Popit
+  class Organization
+    include Popit::OrganizationRepresenter
+  end
+end
---
+++ lib/popit_representers/models/organization_collection.rb
@@ -0,0 +1,7 @@
+require 'popit_representers/representers/organizationcollection_representer'
+
+module Popit
+  class OrganizationCollection
+    include Popit::OrganizationCollectionRepresenter
+  end
+end
